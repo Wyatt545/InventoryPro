8fc3306e3fb16ffd45983ae9afcdb3bbf90f9036
Trenton Scott <trentondanscott@gmail.com>
2020-10-03 13:05:14 -0500

    Login Base
    Implemented base for login system. It successfully logs in as long as you meat email requirements. Working on hooking up DB still.

--- app.py
+++ app.py
@@ -4,20 +4,39 @@ Description: See project readme.
 Notes:
 """
 #==============================Imports=======================================
-import sys
+import sys, os
 
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request, session, redirect, url_for
 from jinja2.exceptions import TemplateNotFound
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 #==============================Routes=======================================
-@app.route("/", methods = ["GET"])
+@app.route("/", methods = ['GET'])
 def index():
-	return render_template("master.html")
+    if (session.get('loggedin') != True):
+        return redirect('/login')
+    else: 
+	    return render_template("master.html")
 
-@app.route("/<route>")
+@app.route("/<route>", methods=['GET', 'POST'])
 def page(route):
+    if (route == 'login') or (session.get('loggedin') != True):
+        if request.method == 'POST':
+            try:
+                session['loggedin'] = True
+                if route == 'login':
+                    return render_template("master.html", page2load='home')
+            except Exception as err:
+                #'An error occurred processing the form. Please try again later. '
+                return render_template("login.html", message=err)
+        else: 
+            return render_template("login.html")
+
+    if route == 'logout':
+        session.pop('loggedin', None)
+        return redirect('/login')
     return render_template("master.html", page2load=route)
 
 @app.route("/page/<route>")
